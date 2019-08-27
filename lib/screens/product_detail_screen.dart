a354e8ab1513832425893ecacfe9f76fa6423b1d
ajay1706 <ajay97456@gmail.com>
2019-08-27 12:31:23 +0530

    adding provider context as products instance

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,5 +1,7 @@
 import 'package:ecomm_app/models/product.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:ecomm_app/providers/products.dart';
 
 class ProductDetailScreen extends StatelessWidget {
 
@@ -12,8 +14,9 @@ static const routeName = '/product-detail';
   @override
   Widget build(BuildContext context) {
    final productId =  ModalRoute.of(context).settings.arguments as String;
-    return Scaffold(
-      appBar: AppBar(title:Text('tiotle'),
+  final loadedProduct =  Provider.of<Products>(context).items.firstWhere((prod) => prod.id == productId);
+   return Scaffold(
+      appBar: AppBar(title:Text(loadedProduct.title),
 
     ));
   }
