8d0a2ae8ef09d7061ab7b67929db319c56c11de1
ajay1706 <ajay97456@gmail.com>
2019-08-29 11:55:06 +0530

    adding alert dialogs to delete

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -10,15 +10,13 @@ class CartItem extends StatelessWidget {
   final String title;
   final String productId;
 
-
   CartItem(
-      this.id,
-      this.price,
-      this.quantity,
-      this.title,
-      this.productId,
-
-      );
+    this.id,
+    this.price,
+    this.quantity,
+    this.title,
+    this.productId,
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -39,6 +37,30 @@ class CartItem extends StatelessWidget {
         ),
       ),
       direction: DismissDirection.endToStart,
+      confirmDismiss: (direction) {
+       return showDialog(
+          context: context,
+          builder: (ctx) => AlertDialog(
+            title: Text(
+              "Are you sure?",
+            ),
+            content: Text("Do you want to remove the item from the cart ?"),
+            actions: <Widget>[
+              FlatButton(
+                child: Text("No"),onPressed: (){
+                  Navigator.of(ctx).pop(false);
+              },
+              ),
+              FlatButton(
+                child: Text("Yes"),onPressed: (){
+                  Navigator.of(context).pop(true);
+              },
+              ),
+            ],
+          ),
+
+        );
+      },
       onDismissed: (direction) {
         Provider.of<Cart>(context, listen: false).removeItem(productId);
       },
